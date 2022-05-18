b35a69c83515dd9c0e0939b6c6ccd25383dddf05
James Macapagal <j.macapagal@gmail.com>
2022-05-18 12:00:22 -0500

    commit tree fec88bc636f5db9dfb68167023db68ab53816596

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers,string){
+  const newArray = drivers.filter(object => object.toUpperCase() === string.toUpperCase());
+  return newArray;
+}
+
+function fuzzyMatch(drivers,string){
+  const newArray = drivers.filter(object => object.charAt(0) === string.charAt(0));
+  return newArray;
+}
+
+function matchName(drivers,string){
+  const newArray = drivers.filter(object => object.name === string);
+  return newArray;
+}
